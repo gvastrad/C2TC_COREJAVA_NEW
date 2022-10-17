4a88bb16be34d4c7fe02194e1ab3c4904f336ac8
HP <HP@LAPTOP-7C3HG0VV>
2022-10-17 18:44:30 +0530

    first commit

--- C2TC_A5/src/StaticDemo.java
+++ C2TC_A5/src/StaticDemo.java
@@ -21,19 +21,25 @@ class Student{
 		int a=12;
 		System.out.println(a);
 		System.out.println(collegeName);
+		//Not static members can't be accessed in static method
+		//System.out.println(id);
+		//Local variables can't be static
+		//static b=22;
+		
 	}
 }
 public class StaticDemo {
 
 	public static void main(String[] args) 
-	{
-		//Student s1=new Student(1,"Shashank");
+	{//Assigning data with constructors
+		Student s1=new Student(1,"Shashank");
 		Student s2=new Student();	
+		//Assigning data with object reference
 		s2.id=2;
 		s2.name="Samruddhi";
 		Student.collegeName="MSRIT";
-		//System.out.println(s1);
-	Student.m1();
+		System.out.println(s1);
+	    Student.m1();
 		System.out.println(s2);
 
 	}
